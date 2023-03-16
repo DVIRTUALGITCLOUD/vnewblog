7565751f4f0a3d2c0a8c35480977d2fec033d14f
MySecoundLanguage <mmamunism@gmail.com>
2023-03-16 12:24:15 +0600

    Fixed

--- blugz/models.py
+++ blugz/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default='coding')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='blog_posts')
     
     def total_likes(self):
